82f3c827d4d59bcb579814ff46000f790112a99b
andrewmcvearry <andrew.mcvearry@gmail.com>
2014-05-13 12:47:48 -0400

    Create Deck class outline

---
+++ Deck.java
@@ -0,0 +1,25 @@
+public class Deck
+{
+    private ArrayList<Card> cards;
+    
+    public Deck()
+    {
+        // fill in all Mille Bornes cards here
+        // look on wikibook etc.
+    }
+    
+    public void shuffle()
+    {
+        // rearrange all the cards in the deck
+    }
+    
+    public Card getTopCard()
+    {
+        // returns the card at the top of the cards ArrayList
+    }
+    
+    public Card takeTopCard()
+    {
+        // returns the top card and removes it from the Deck
+    }
+}
