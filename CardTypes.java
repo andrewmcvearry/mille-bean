b7b36ed5f59d94bd0d733ff5f0cd4d3146689573
andrewmcvearry <andrew.mcvearry@gmail.com>
2014-05-15 08:57:40 -0400

    first types in CardTypes.java

---
+++ CardTypes.java
@@ -0,0 +1,15 @@
+public abstract class HazardCard extends Card
+{
+    public HazardCard(String name, CardImage image)
+    {
+        super(name, image);
+    }
+}
+
+public abstract class RemedyCard extends Card
+{
+    public RemedyCard(String name, CardImage image)
+    {
+        super(name, image);
+    }
+}
