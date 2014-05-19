78e1607cc6459f5cd52b438d83ee997c561528c5
Andrew McVearry <andrew.mcvearry@gmail.com>
2014-05-18 22:19:38 -0400

    added NetBeans project files

---
+++ src/Card.java
@@ -0,0 +1,11 @@
+public abstract class Card
+{
+    private String name;
+    private CardImage image;
+    
+    public Card(String n, CardImage i)
+    {
+        name = n;
+        image = i;
+    }
+}
