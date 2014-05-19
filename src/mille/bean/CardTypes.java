dc538ff11f2062412f980f350642caf6f89570a4
5amcvear <5amcvear@10.1.200.179>
2014-05-19 08:15:18 -0400

    add HazardCard subtypes

--- src/mille/bean/CardTypes.java
+++ src/mille/bean/CardTypes.java
@@ -31,3 +31,43 @@ public abstract class DistanceCard extends Card
         super(name, image);
     }
 }
+
+public class AccidentCard extends HazardCard
+{
+    public AccidentCard(CardImage image)
+    {
+        super("Accident", image);
+    }
+}
+
+public class OutOfGasCard extends HazardCard
+{
+    public OutOfGasCard(CardImage image)
+    {
+        super("Out of Gas", image);
+    }
+}
+
+public class FlatTireCard extends HazardCard
+{
+    public FlatTireCard(CardImage image)
+    {
+        super("Flat Tire", image);
+    }
+}
+
+public class SpeedLimitCard extends HazardCard
+{
+    public SpeedLimitCard(CardImage image)
+    {
+        super("Speed Limit", image);
+    }
+}
+
+public class StopCard extends HazardCard
+{
+    public StopCard(CardImage image)
+    {
+        super("Stop", image);
+    }
+}
