9444a0f22684fd4912664cb5b9b893c1cb7f2e51
Andrew McVearry <andrew.mcvearry@gmail.com>
2014-05-21 08:07:21 -0400

    add cards to deck class

--- src/mille/bean/Deck.java
+++ src/mille/bean/Deck.java
@@ -11,7 +11,76 @@ public class Deck
         // TODO: change this
         CardImage defaultImage = new CardImage();
         
-        cards.add(new AccidentCard(defaultImage));
+        for (int i = 0; i < 3; i++) {
+            cards.add(new AccidentCard(defaultImage));
+        }
+        
+        for (int i = 0; i < 3; i++) {
+            cards.add(new OutOfGasCard(defaultImage));
+        }
+        
+        for (int i = 0; i < 3; i++) {
+            cards.add(new FlatTireCard(defaultImage));
+        }
+        
+        for (int i = 0; i < 4; i++) {
+            cards.add(new SpeedLimitCard(defaultImage));
+        }
+        
+        for (int i = 0; i < 5; i++) {
+            cards.add(new StopCard(defaultImage));
+        }
+        
+        for (int i = 0; i < 6; i++) {
+            cards.add(new RepairsCard(defaultImage));
+        }
+        
+        for (int i = 0; i < 6; i++) {
+            cards.add(new GasolineCard(defaultImage));
+        }
+        
+        for (int i = 0; i < 6; i++) {
+            cards.add(new SpareTireCard(defaultImage));
+        }
+        
+        for (int i = 0; i < 6; i++) {
+            cards.add(new EndOfLimitCard(defaultImage));
+        }
+        
+        for (int i = 0; i < 14; i++) {
+            cards.add(new RollCard(defaultImage));
+        }
+        
+        cards.add(new DrivingAceCard(defaultImage));
+        cards.add(new ExtraTankCard(defaultImage));
+        cards.add(new PunctureProofCard(defaultImage));
+        cards.add(new RightOfWayCard(defaultImage));
+        
+        for (int i = 0; i < 10; i++)
+        {
+            cards.add(new Distance25Card(defaultImage));
+        }
+        
+        for (int i = 0; i < 10; i++)
+        {
+            cards.add(new Distance50Card(defaultImage));
+        }
+        
+        for (int i = 0; i < 10; i++)
+        {
+            cards.add(new Distance75Card(defaultImage));
+        }
+        
+        for (int i = 0; i < 12; i++)
+        {
+            cards.add(new Distance100Card(defaultImage));
+        }
+        
+        for (int i = 0; i < 4; i++)
+        {
+            cards.add(new Distance200Card(defaultImage));
+        }
+        
         // fill in all Mille Bornes cards here
         // look on wikibook etc.
     }
