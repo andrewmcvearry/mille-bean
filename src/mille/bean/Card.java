3d0a015a164468b1259ea246886ca33e4f780756
Andrew McVearry <andrew.mcvearry@gmail.com>
2014-05-23 08:30:54 -0400

    add get methods to Card

--- src/mille/bean/Card.java
+++ src/mille/bean/Card.java
@@ -2,12 +2,22 @@ package mille.bean;
 
 public abstract class Card
 {
-    private String name;
-    private CardImage image;
+    private final String name;
+    private final CardImage image;
     
     public Card(String n, CardImage i)
     {
         name = n;
         image = i;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public CardImage getCardImage()
+    {
+        return image;
     }
 }
