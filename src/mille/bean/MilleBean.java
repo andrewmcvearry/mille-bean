930d388a2c660f6ee2e2e48aa97167d22d0c5cd3
Andrew McVearry <andrew.mcvearry@gmail.com>
2014-06-08 21:23:19 -0400

    updateDisplay() now clears the window to a brown color

--- src/mille/bean/MilleBean.java
+++ src/mille/bean/MilleBean.java
@@ -81,17 +81,20 @@ public class MilleBean {
     public static void updateDisplay()
     {
         // Clear the screen and depth buffer
+        GL11.glClearColor(0.5f, 0.3f, 0.1f, 0.0f);
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-
-        // set the color of the quad (R,G,B,A)
-        GL11.glColor3f(0.5f, 0.5f, 1.0f);
-
-        // draw quad
-        GL11.glBegin(GL11.GL_QUADS);
-        GL11.glVertex2f(100, 100);
-        GL11.glVertex2f(100 + 200, 100);
-        GL11.glVertex2f(100 + 200, 100 + 200);
-        GL11.glVertex2f(100, 100 + 200);
-        GL11.glEnd();
+        
+//        // set the color of the quad (R,G,B,A)
+//        GL11.glColor3f(0.5f, 0.5f, 1.0f);
+//
+//        // draw quad
+//        GL11.glBegin(GL11.GL_QUADS);
+//        GL11.glVertex2f(100, 100);
+//        GL11.glVertex2f(100 + 200, 100);
+//        GL11.glVertex2f(100 + 200, 100 + 200);
+//        GL11.glVertex2f(100, 100 + 200);
+//        GL11.glEnd();
+        
+        Display.update();
     }
 }
